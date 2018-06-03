5ab472411c086bc92cd8da1cb027e00268d55705
juzhuoyue <juzhuoyue>
2018-06-03 11:32:54 +0800

    datadisplay-master/

---
+++ datadisplay-master/src/main/java/top/jzy/datadisplay/services/xinxiService.java
@@ -0,0 +1,16 @@
+package top.jzy.datadisplay.services;
+
+import top.jzy.datadisplay.domain.xinxiDO;
+
+import java.util.List;
+
+/**
+ * 学生信息列表
+ * service层
+ *
+ * @author WarMj
+ * @date 2018-05-16
+ */
+public interface xinxiService {
+  List<xinxiDO> listxinxi();
+}
